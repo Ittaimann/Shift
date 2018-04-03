3d4a38705ad1501c70e219a4f8bc9d69a9afdcc9
jwardlaw <jwardlaw.n@gmail.com>
2018-04-03 16:55:16 -0700

    Made a scripts folder, added some shit to change orientations

---
+++ Shift/Assets/Scripts/ShiftOrientation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{
+	none,
+	forward,
+	backward,
+	left,
+	right
+}
+
+public class ShiftOrientation : MonoBehaviour 
+{
+	public Direction movement;
+
+	public void Shift()
+	{
+		switch(movement)
+		{
+			case Direction.none:
+			break;
+			case Direction.forward:
+			this.transform.Rotate(Vector3.right * 90f);
+			break;
+			case Direction.backward:
+			this.transform.Rotate(Vector3.left * 90f);
+			break;
+			case Direction.left:
+			this.transform.Rotate(Vector3.forward * 90f);
+			break;
+			case Direction.right:
+			this.transform.Rotate(Vector3.back * 90f);
+			break;
+		}
+	}
+}
--- Shift/Assets/Scripts/movement.cs
+++ Shift/Assets/Scripts/movement.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class movement : MonoBehaviour
 {
+	public ShiftOrientation shifter;
     public float speed = 6.0F;
     public float jumpSpeed = 8.0F;
-    public float gravity = 20.0F;
+    public float gravityForce = 20.0F;
 	private Rigidbody rb;
+	private Vector3 gravityVector;
 
     float rotationY = 0F;
 	public float MinY;
@@ -21,18 +23,19 @@ public class movement : MonoBehaviour
 	void Awake()
 	{
 		rb = GetComponent<Rigidbody>();
+		shifter = GetComponent<ShiftOrientation>();
 	
 	}
 	void Start()
 	{
+		Cursor.lockState = CursorLockMode.Locked;
         cam = Camera.main.gameObject;
 	}
 
 	void Update()
 	{
+
         Quaternion deltaRotation = Quaternion.Euler(0,Input.GetAxis("Mouse X")*SensitivityX,0);
-
-
 
 
         rotationY +=Input.GetAxis("Mouse Y")*SensitivityY;
@@ -42,13 +45,24 @@ public class movement : MonoBehaviour
 		cam.transform.localEulerAngles = new Vector3( -rotationY, 0, 0 );
 
 
-	
-
 		rb.MoveRotation(Quaternion.Euler( rb.rotation.eulerAngles + deltaRotation.eulerAngles ));
 
 		
-		
-		
+		if(Input.anyKeyDown)
+		{
+			if(Input.GetKeyDown(KeyCode.W))
+				shifter.movement = Direction.forward;
+			if(Input.GetKeyDown(KeyCode.S))
+				shifter.movement = Direction.backward;
+			if(Input.GetKeyDown(KeyCode.A))
+				shifter.movement = Direction.left;
+			if(Input.GetKeyDown(KeyCode.D))
+				shifter.movement = Direction.right;
+			if(Input.GetKeyDown(KeyCode.LeftShift))
+			{
+				shifter.Shift();
+			}
+		}
 		rb.velocity=(transform.right*Input.GetAxisRaw("Horizontal") +transform.forward* Input.GetAxisRaw("Vertical"))*speed;
 	}
 }
